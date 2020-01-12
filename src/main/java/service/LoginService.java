3e62bc978b5c8eb8c56e5046166426c1404f3d05
MikiKru <michal_kruczkowski@o2.pl>
2020-01-12 13:45:15 +0100

    Dodanie użytkowników w utility

---
+++ src/main/java/service/LoginService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.User;
+
+import java.util.Optional;
+
+// klasa implementująca logikę biznesową aplikacji
+public class LoginService {
+    // metoda do logowania użytkownika
+    public Optional<User> loginUser(String login, String password){
+        return null;
+    }
+}
