36740d2f72983f13a2db2683860adcdd8ca62e43
MikiKru <michal_kruczkowski@o2.pl>
2020-01-12 13:34:09 +0100

    Dodanie modelu User i enuma Role

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
+        primaryStage.setTitle("panel logowania");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);           // blokowanie zmiany rozmiaru okna
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
+
         launch(args);
     }
 }
