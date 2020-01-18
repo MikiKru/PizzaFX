cc1e133b496c1a2e00a6276b73ab377e33ced2e1
MikiKru <michal_kruczkowski@o2.pl>
2020-01-18 14:40:05 +0100

    registration with file DB

--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -1,6 +1,18 @@
 package service;
 
+import java.io.File;
+
 // logika biznesowa do obsługi plików
 public class FileService {
+    private String pathToUsers = "???";
+    // metoda pobierająca zawartość z pliku i wprawadzająca ją do listy users
+    public void selectUser(){
+        File file = new File(pathToUsers);
 
+
+    }
+    // metoda zapisująca zawartość z listy users do pliku
+    public void insertUsers(){
+
+    }
 }
