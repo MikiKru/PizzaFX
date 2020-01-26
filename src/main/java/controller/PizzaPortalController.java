27c86919753628475b224c1bc49252756619c1fa
MikiKru <michal_kruczkowski@o2.pl>
2020-01-26 09:15:31 +0100

    pizza portal - admin

--- src/main/java/controller/PizzaPortalController.java
+++ src/main/java/controller/PizzaPortalController.java
@@ -10,8 +10,10 @@ import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import model.Basket;
 import model.Pizza;
 import model.PizzaList;
+import model.Status;
 import service.LoginService;
 import service.PizzaPortalService;
 import service.WindowService;
@@ -19,10 +21,14 @@ import utility.InMemoryDb;
 
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PizzaPortalController {
+    // TAB 1 - PIZZA --------------------------------------------------------------------
     // obiekty globalne
     private WindowService windowService;
     private PizzaPortalService pizzaPortalService;
@@ -32,7 +38,7 @@ public class PizzaPortalController {
     private Label lblLogin;
     @FXML
     private Tab tabMenu;
-// ----------------------- LIST PIZZ -----------------------------
+    // ----------------------- LIST PIZZ -----------------------------
     @FXML
     private TableView<PizzaList> tblPizza;
     @FXML
@@ -45,14 +51,14 @@ public class PizzaPortalController {
     private TableColumn<PizzaList, Double> tcPrice;
     @FXML
     private TableColumn<PizzaList, Integer> tcQuantity;
-// ---------------------------------------------------------------
+    // ---------------------------------------------------------------
     @FXML
     private Label lblSum;
 
     @FXML
     private Label lblPizzaOfDay;
 
-    private void clearOrder(){
+    private void clearOrder() {
         List<PizzaList> pizzaLists = pizzaPortalService.clearPizzaOrder();
         pizzas.clear();
         pizzas.addAll(pizzaLists);
@@ -63,7 +69,7 @@ public class PizzaPortalController {
 
     @FXML
     void addToBasketAction(ActionEvent event) throws IOException {
-        if(pizzaPortalService.calculatePizzaOrder() > 0) {
+        if (pizzaPortalService.calculatePizzaOrder() > 0) {
             pizzaPortalService.addOrderToBasket(LoginService.loggedUser.getLogin()); // login zalogowanego użytkownika
             clearOrder();
             WindowService.getAlertWindow(
@@ -71,6 +77,8 @@ public class PizzaPortalController {
                     "Dodawanie do koszyka",
                     "Złożono zamówienie",
                     "Dziękujemy za złożenie zamówienia. Możesz w zakładce koszyk śledzić jego status");
+            // aktualizacja tablicy koszyków
+            addDataToBasketsTable();
         } else {
             WindowService.getAlertWindow(
                     AlertType.WARNING,
@@ -79,10 +87,12 @@ public class PizzaPortalController {
                     "Musisz wybrać jakiś produkt aby zrealizować zamówienie");
         }
     }
+
     @FXML
     void clearAction(ActionEvent event) {
         clearOrder();
     }
+
     @FXML
     void logoutAction(ActionEvent event) throws IOException {
         windowService.createWindow("loginView", "panel logowania");    // otwarcie okna logowania
@@ -95,8 +105,6 @@ public class PizzaPortalController {
         Platform.exit();
     }
 
-
-
     @FXML
     void selectPizzaAction(MouseEvent event) {
         List<PizzaList> pizzaLists = pizzaPortalService.selectPizza(tblPizza);
@@ -108,8 +116,83 @@ public class PizzaPortalController {
         lblSum.setText(String.format("do zapłaty: %.2f zł", pizzaPortalService.calculatePizzaOrder()));
     }
 
+    // TAB 2 - BASKET --------------------------------------------------------------------
+    @FXML
+    private TableView<Basket> tblBasket;
+    @FXML
+    private TableColumn<Basket, Map> tcBasket;
+    @FXML
+    private TableColumn<Basket, Status> tcStatus;
+    @FXML
+    private ListView<String> lvBasket;
+    @FXML
+    private Label lblBasketAmount;
+    @FXML
+    void showDetailsAction(MouseEvent event) {
+        // zaznaczamy rekord w tabelce i pobieramy z niego obiekt klasy Basket
+        Basket basket = tblBasket.getSelectionModel().getSelectedItem();
+        // wypisanie szczegółowych informacji dot zaznaczonego koszyka
+        ObservableList<String> detailBasket = FXCollections.observableArrayList();
+        detailBasket.add("STATUS: " + basket.getStatus().getStatusName());
+        for(String name : basket.getOrder().keySet()){
+            detailBasket.add("Pizza: " + name + " : " + basket.getOrder().get(name) + "szt.");
+        }
+        lvBasket.setItems(detailBasket);
+        // aktualizacja kwoty do zapłaty
+        lblBasketAmount.setText(String.format("SUMA: %.2f PLN", basket.getBasketAmount()));
+    }
+    // metoda wprowadzająca dane z pliku baskets.csv do koszyka,
+    // ale dotyczące tylko zalogowanego użytkownika
+    private List<Basket> getUserBasket(String login){
+        // 1. z listy koszyków odfiltruj tylko dane dot. usera zidentyfikowanego po loginie
+        List<Basket> userBaskets = InMemoryDb.baskets.stream()
+                .filter(basket -> basket.getUserLogin().equals(login))
+                .sorted(Comparator.comparing(Basket::getStatus))
+                .collect(Collectors.toList());
+        return userBaskets;
+    }
+    // metoda do konfiguracji kolumn w tblBasket i wprowadzenia danych dot. koszyków użytkownika
+    private void addDataToBasketsTable(){
+        // konfiguracja wartości przekazywanych do kolumn z modelu
+        tcBasket.setCellValueFactory(new PropertyValueFactory<>("order"));
+        tcStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        // formatowanie zawartości tabeli
+        tcBasket.setCellFactory(order -> new TableCell<Basket, Map>() {
+            @Override
+            protected void updateItem(Map basket, boolean empty) {
+                super.updateItem(basket, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    setText(basket.toString()
+                            .replace("{", "")
+                            .replace("}", "")
+                            .replace("=", " x ")
+                    );
+                }
+            }
+        });
+        tcStatus.setCellFactory(order -> new TableCell<Basket, Status>() {
+            @Override
+            protected void updateItem(Status status, boolean empty) {
+                super.updateItem(status, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    setText(status.getStatusName());
+                }
+            }
+        });
+        // wprowadzenie danych do tabeli z listy baskets
+        tblBasket.setItems(FXCollections.observableArrayList(
+                getUserBasket(LoginService.loggedUser.getLogin())
+        ));
+    }
+
+    // -----------------------------------------------------------------------------------
     private PizzaList pizzaOfDay;
-    public void initialize(){
+
+    public void initialize() {
         // wypisanie loginu zalogowanego użytkownika na lbl
         lblLogin.setText("zalogowano: " + LoginService.loggedUser.getLogin());
         // ---------------------------------------------------------
@@ -126,7 +209,7 @@ public class PizzaPortalController {
         lblPizzaOfDay.setText("PIZZA DNIA TO " + pizzaOfDay.getName().toUpperCase() + "!");
         // konfiguracja wartości wprowadzanych do kolumn tblPizza
         tcName.setCellValueFactory(new PropertyValueFactory<PizzaList, String>("name"));
-        tcIngredients.setCellValueFactory(new PropertyValueFactory<PizzaList,String>("ingredients"));
+        tcIngredients.setCellValueFactory(new PropertyValueFactory<PizzaList, String>("ingredients"));
         tcDescription.setCellValueFactory(new PropertyValueFactory<PizzaList, String>("description"));
         tcPrice.setCellValueFactory(new PropertyValueFactory<PizzaList, Double>("price"));
         tcQuantity.setCellValueFactory(new PropertyValueFactory<PizzaList, Integer>("quantity"));
@@ -146,6 +229,9 @@ public class PizzaPortalController {
         });
         // wprowadzenie wartości do tbl
         tblPizza.setItems(pizzas);
+        // TAB2 - BASKET --------------------------------------------------
+        addDataToBasketsTable();
+        // ----------------------------------------------------------------
     }
 
 }
